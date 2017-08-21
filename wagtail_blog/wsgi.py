94d894f889971d1d09c662c2ad1357c4e2345013
cityblack1 <690931381@qq.com>
2017-08-21 11:21:09 +0800

    ready for pro

--- wagtail_blog/wsgi.py
+++ wagtail_blog/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_blog.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_blog.settings.production")
 
 application = get_wsgi_application()
